b2d8bfa7fffa35922aa9198016b59c00f6fd85ea
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 22:52:32 +0300

    Add project files.

---
+++ Proces_ile_hesap_makinesi/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Proces_ile_hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.process1 = new System.Diagnostics.Process();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_acilacak_dosya = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(158, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 119);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hesap Makinesi Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_acilacak_dosya
+            // 
+            this.txt_acilacak_dosya.Location = new System.Drawing.Point(507, 258);
+            this.txt_acilacak_dosya.Name = "txt_acilacak_dosya";
+            this.txt_acilacak_dosya.Size = new System.Drawing.Size(100, 20);
+            this.txt_acilacak_dosya.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Yeni dosyayı aç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 420);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_acilacak_dosya);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_acilacak_dosya;
+    }
+}
+
---
+++ Proces_ile_hesap_makinesi/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proces_ile_hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            process1.StartInfo.FileName = "calc.exe";
+            process1.Start();
+            //bu yolla bilgisayardaki farklı şeylere de ulaşılabilir...
+            //chrome.exe yazılınca mesala chrome açar hemen...
+            // mesala: "D:\\YEDEK\\ornekklasör\ornekdosya.txt"
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            process1.StartInfo.FileName = txt_acilacak_dosya.Text;
+            process1.Start();
+        }
+    }
+}
